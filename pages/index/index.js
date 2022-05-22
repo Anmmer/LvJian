b2e26eb4d4f4db308f1ac55a41da30e3c3445736
28683 <2868310944@qq.com>
2022-05-22 16:47:15 +0800

    commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -77,7 +77,8 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    // canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    canIUseOpenData:false,
   },
   onLoad() {
     let on_or_off
